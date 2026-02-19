d7b6eaeacc7444af1906cd092812e3b8e2536f30
tomimandalaputra <tommymandalaputra@gmail.com>
2026-02-19 13:07:13 +0700

    setup database connection and logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/tomimandalaputra/e-commerce-go/internal/config"
+	"github.com/tomimandalaputra/e-commerce-go/internal/database"
+	"github.com/tomimandalaputra/e-commerce-go/internal/logger"
+)
 
 func main() {
-	fmt.Print("Hello world")
+	log := logger.New()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+
+	db, err := database.New(&cfg.Database)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to database")
+	}
+
+	mainDB, err := db.DB()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get database connection")
+	}
+	defer mainDB.Close()
+
+	gin.SetMode(cfg.Server.GinMode)
+
+	log.Info().Msg("Starting server")
 }
