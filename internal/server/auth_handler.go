b115d81e69925da43a9472c3a22c1109d802b841
tomimandalaputra <tommymandalaputra@gmail.com>
2026-02-24 12:39:29 +0700

    added user handler

--- internal/server/auth_handler.go
+++ internal/server/auth_handler.go
@@ -72,3 +72,33 @@ func (s *Server) logout(c *gin.Context) {
 
 	utils.SuccessResponse(c, "Logout successful", nil)
 }
+
+func (s *Server) getProfile(c *gin.Context) {
+	userID := c.GetUint("user_id")
+	userService := services.NewUserService(s.db)
+	profile, err := userService.GetProfile(userID)
+	if err != nil {
+		utils.NotFoundResponse(c, "User not found")
+		return
+	}
+
+	utils.SuccessResponse(c, "Profile retrieved successfully", profile)
+}
+
+func (s *Server) updateProfile(c *gin.Context) {
+	userID := c.GetUint("user_id")
+
+	var req dto.UpdateProfileRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		utils.BadRequestResponse(c, "Invalid request data", err)
+	}
+
+	userService := services.NewUserService(s.db)
+	profile, err := userService.UpdateProfile(userID, &req)
+	if err != nil {
+		utils.InternalServerErrorResponse(c, "Failed to update profile", err)
+		return
+	}
+
+	utils.SuccessResponse(c, "Prolie update successfully", profile)
+}
